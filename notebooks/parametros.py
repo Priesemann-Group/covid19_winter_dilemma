0324f59b30e5aea8451813c98b925047b789ef82
Philipp Dönges <philipp.doenges@stud.uni-goettingen.de>
2021-10-02 14:35:10 +0200

    parametros update, first part

--- notebooks/parametros.py
+++ notebooks/parametros.py
@@ -1,23 +1,16 @@
 import numpy as np
 
-y0 = {
-    'S': 276621.,
-    'V': 599864.,
-    'W':  97500.,
-    'E':    390.,
-    'EB':    39.,
-    'I':    974.,
-    'IB':    97.,
-    'ICU':   15.,
-    'R':  24500.,
-    'UC':599864+97500,
-    'WC': 599864/100,
-    'D': 0.,
-    'C': 0.,
-}
+# Values from OWD
+V_raw = 603900.0
+R_raw =  47400.0
+cases =    116.25
+ICU =       13.46
+W_V =    18000.0
+W_R =     9030.0
+darkfigure = 2.5
 
 params = {
-    'y0': list(y0.values()),
+#    'y0': y0_array,
     'Rt_base': 3.25,
     'Rt_free': 5,
     'eta': 0.75,
@@ -55,6 +48,39 @@ params = {
     'influx': 1,
     'epsilon_free':14.,
 }
+
+y0 = {
+    'V': V_raw - W_V,
+    'R': darkfigure * (R_raw-W_R),
+    'W': W_V + darkfigure*W_R,
+    'ICU': ICU,
+}
+
+E_stay = 1./params['rho']
+I_stay = 1./(params['gamma']+params['delta']+params['Theta'])
+
+immune_infected = (y0['R']+y0['V'])*(1-params['eta'])
+S_approx = 1e6 - sum(list(y0.values())) - darkfigure*(E_stay+I_stay)*cases
+breakthrough = immune_infected / (immune_infected + S_approx)
+
+y0.update({
+    'E': darkfigure*cases*E_stay*(1-breakthrough),
+    'EB': darkfigure*cases*E_stay*breakthrough,
+    'I': darkfigure*cases*I_stay*(1-breakthrough),
+    'IB': darkfigure*cases*I_stay*breakthrough,
+})
+
+y0['S'] = 1e6 - sum(list(y0.values()))
+
+y0.update({
+    'UC': V_raw,
+    'WC': 0.,
+    'D': 0.,
+    'C': 0.,
+})
+
+y0_array = [y0['S'],y0['V'],y0['W'],y0['E'],y0['EB'],y0['I'],y0['IB'],y0['ICU'],y0['R'],y0['UC'],y0['WC'],y0['D'],y0['C']]
+params['y0'] = y0_array
 
 
 Rtbase = {
