8f801049c01cce9a332a5e5befa9198361e61286
Philipp Dönges <philipp.doenges@stud.uni-goettingen.de>
2021-11-02 11:11:18 +0100

    softplus added, code could need some cleanup

--- code/model_agegroups.py
+++ code/model_agegroups.py
@@ -106,6 +106,9 @@ class Model:
     def time2index(self, t):
         return round((t+self.t_min)/self.step_size)
 
+    def softplus(self, slope, base, threshold, epsilon):
+        return lambda x: slope*epsilon*np.log(np.exp(1/epsilon*(threshold-x))+1) + base
+
     def H(self,t,tau,a,b):
         x = -np.arange(-self.gamma_cutoff,0,self.step_size)
         d = self.data[self.time2index(t-self.gamma_cutoff-tau):self.time2index(t-tau),10:12].sum(axis=1)
@@ -127,28 +130,13 @@ class Model:
 
     def Gamma(self, t):
         return 1 + self.mu*np.cos(2*np.pi*(t+self.d_0-self.d_mu)/360.)
-
-
-    #def C_npi(self, t):
-    #    if t<180-self.epsilon_free:
-    #        return self.C_npi_base
-    #    if t>180+self.epsilon_free:
-    #        return self.C_npi_free
-    #    else:
-    #        return self.C_npi_base + (self.C_npi_free-self.C_npi_base)*(t-180+self.epsilon_free)/2/self.epsilon_free
-
-    #Reproduction Number Old    
-    #def Rt(self, t):
-     #   return self.R_0(t)*np.exp(-self.alpha_R*self.H_Rt(t)-self.e_R) * self.Gamma(t) /self.Gamma(360-self.d_0)
-    
-
     
     #Cosmo Data fit 
-    def selfregulation(self, t):
-        return self.plateaus - self.slopes*self.fit_epsilon*np.log(np.e**(1/self.fit_epsilon*(self.fit_ICUcap-self.H_Rt(t)))+1)
+#    def selfregulation(self, t):
+#        return self.plateaus - self.slopes*self.fit_epsilon*np.log(np.e**(1/self.fit_epsilon*(self.fit_ICUcap-self.H_Rt(t)))+1)
 
-    def alpha_vol(self,t):
-        return (1-(self.selfregulation(t)-self.mappingparam)/(5-self.mappingparam))
+#    def alpha_vol(self,t):
+#        return (1-(self.selfregulation(t)-self.mappingparam)/(5-self.mappingparam))
 
     def fit_HR(self, t):
         if t<180-self.epsilon_free:
@@ -166,42 +154,42 @@ class Model:
             return self.fit_LR_reduced + (self.fit_LR_free-self.fit_LR_reduced)*(t-180+self.epsilon_free)/2/self.epsilon_free
 
     def new_sr(self,t):
-        a = -(self.fit_LR(t)-self.fit_HR(t))/self.fit_ICUcap
-        b = self.fit_LR(t)-self.fit_HR(t)
+#        a = -(self.fit_LR(t)-self.fit_HR(t))/self.fit_ICUcap
+#        b = self.fit_LR(t)-self.fit_HR(t)
+#        res =  (self.H_Rt(t)[0]<self.fit_ICUcap) * (a*self.H_Rt(t)[0]+b) + self.fit_HR(t)
+        a = (self.fit_LR(t)-self.fit_HR(t))/self.fit_ICUcap
         # ugly hack: just choose the first entry of H_Rt, FIX IT LATER!
-        res =  (self.H_Rt(t)[0]<self.fit_ICUcap) * (a*self.H_Rt(t)[0]+b) + self.fit_HR(t)
+        res = self.softplus(a, self.fit_HR(t), self.fit_ICUcap, 10)(self.H_Rt(t)[0])
         # scale down households
         res[0] *= res[1:].sum()/3.
         return res
-
+ 
     def Rt(self, t): # not directly used in the code, for plotting only
         CM = np.zeros([6,6])
         for i in range(4):
             CM += self.Cs[i,:,:] * self.new_sr(t)[i]
         return self.R0 * self.Gamma(t) * max(np.linalg.eigvals(CM))
-            
+
     def u_w(self, t):
         return self.u_base + (self.u_max-self.u_base)*(1-np.exp(-self.alpha_u*self.H_vac1(t)-self.e_u))
 
     def Phi(self, t, UC, frac):
-        ppl = (self.u_w(t))*self.M - UC
-        return ( ppl > 0 ) * ppl * frac / self.time_u
+#        ppl = (self.u_w(t))*self.M - UC
+#        return ( ppl > 0 ) * ppl * frac / self.time_u
+        return self.softplus(frac/self.time_u, 0, self.u_w(t), 0.01)(UC/self.M) * self.M
 
     def w_w(self, t):
         return self.w_max*(1-np.exp(-self.alpha_w*self.H_vac2(t)-self.e_w))
 
     def phi(self, t, UC, WC, frac):
-        ppl = ((self.w_w(t))*UC - WC)
-        return ( ppl > 0 ) * ppl * frac / self.time_w
+#        ppl = ((self.w_w(t))*UC - WC)
+#        return ( ppl > 0 ) * ppl * frac / self.time_w
+        return self.softplus(frac/self.time_w, 0, self.w_w(t), 0.01)(WC/UC) * UC
 
     def get_phis(self, t, y):   # for plotting only right now; TODO later: check speed and use if not to slow
         (S,V,Wn,Wv,E,EBn,EBv,I,IBn,IBv,ICU,ICUv,R,Rv,UC,WC,D,C) = np.split(y, self.eqs)
         Phi = self.Phi(t, UC, (S+Wn)/(self.M-UC))
         phi = self.phi(t, UC, WC, (Wv)/(UC-WC))
-#        ratio = (Phi+phi).sum() / (self.vac_max*self.M.sum())
-#        if ratio > 1:
-#            Phi /= ratio
-#            phi /= ratio
         return Phi, phi
 
     # ACHTUNG: No reduced waning for high incidences at the moment
@@ -250,15 +238,15 @@ class Model:
         dWn = -Wn*infect + omega_n*R - Phi*(Wn/(S+Wn))
         dWv = -Wv*infect + omega_v*V + omega_n*Rv - phi
         dE = S*infect - rho*E
-        dEBn = Wn*infect - rho*EBn
-        dEBv = ((1-eta)*V+Wv)*infect - rho*EBv
+        dEBn = Wn*infect + R*(1-eta)*infect - rho*EBn
+        dEBv = ((1-eta)*(V+Rv)+Wv)*infect - rho*EBv
         dI = rho*E - (gamma+delta+Theta)*I
         dIBn = rho*EBn - (gamma + (Theta+delta)*(1-kappa))*IBn
         dIBv = rho*EBv - (gamma + (Theta+delta)*(1-kappa))*IBv
         dICU = delta*(I + (1-kappa)*IBn) - (Theta_ICU+gamma_ICU)*ICU
         dICUv = delta*(1-kappa)*IBv - (Theta_ICU+gamma_ICU)*ICUv
-        dR = gamma*(I+IBn) - omega_n*R + gamma_ICU*ICU
-        dRv = gamma*IBv - omega_n*Rv + gamma_ICU*ICUv
+        dR = gamma*(I+IBn) - omega_n*R + gamma_ICU*ICU - R*(1-eta)*infect
+        dRv = gamma*IBv - omega_n*Rv + gamma_ICU*ICUv - Rv*(1-eta)*infect
         dUC = Phi
         dWC = phi
         dD = Theta*I + (1-kappa)*Theta*(IBn+IBv) + Theta_ICU*(ICU+ICUv)
