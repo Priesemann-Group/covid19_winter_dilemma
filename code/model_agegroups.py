f18ad8483cedf030d0d13f41f52b7cb869d29e3c
Philipp Dönges <philipp.doenges@stud.uni-goettingen.de>
2021-11-09 12:43:16 +0100

    code cleanup

--- code/model_agegroups.py
+++ code/model_agegroups.py
@@ -7,56 +7,52 @@ import pickle
 class Model:
     def __init__(
         self,
-        y0, R0,
-        eta, kappa, sigma,
+
+        y0, Cs,
+        beta, kappa, sigma, delta, rho,
         gamma, gamma_ICU,
-        delta,
-        rho,
-        omega_v_b, omega_n_b,
-        chi_0, chi_1,
-        alpha_w, alpha_u, alpha_R,
-        e_R, e_u, e_w,
-        vac_max,
-        u_base,
+        Theta, Theta_ICU,
+        omega_v, omega_n,
+
         mu, d_0, d_mu,
+
         a_Rt, b_Rt, a_vac, b_vac, gamma_cutoff,
         tau_vac1, tau_vac2,
-        t_max, step_size,
-        Theta, Theta_ICU,
-        influx,
+
+        k_ICUcap, epsilon_k,
+        k_lowH_NPI, k_highH_NPI,
+        k_lowH_noNPI, k_highH_noNPI,
+
+        alpha_u, alpha_w,
+        u_base, w_base,
+        chi_u, chi_w,
         time_u, time_w,
+        epsilon_u, epsilon_w,
+
         epsilon_free,
-        C_base, C_npi_base, C_npi_free, C_vol,
-        w_ICU,
-        plateaus, slopes,
-        feedback_off, mappingparam, fit_epsilon,
-        fit_ICUcap, fit_HR_reduced, fit_LR_reduced, fit_HR_free, fit_LR_free,
-        Cs
+        influx,
+        t_max, step_size,
+        feedback_off,
     ):
         self.y0 = y0
-        self.R0 = R0
-        self.eta = eta
+        self.Cs = Cs
+
+        self.beta = beta
         self.kappa = kappa
         self.sigma = sigma
-        self.gamma = gamma
-        self.gamma_ICU = gamma_ICU
         self.delta = delta
         self.rho = rho
-        self.omega_v_b = omega_v_b
-        self.omega_n_b = omega_n_b
-        self.chi_0 = chi_0
-        self.chi_1 = chi_1
-        self.alpha_w = alpha_w
-        self.alpha_u = alpha_u
-        self.alpha_R = alpha_R
-        self.e_R = e_R
-        self.e_u = e_u
-        self.e_w = e_w
-        self.vac_max = vac_max
-        self.u_base = u_base
+        self.gamma = gamma
+        self.gamma_ICU = gamma_ICU
+        self.Theta = Theta
+        self.Theta_ICU = Theta_ICU
+        self.omega_v = omega_v
+        self.omega_n = omega_n
+
         self.mu = mu
         self.d_0 = d_0
         self.d_mu = d_mu
+
         self.a_Rt = a_Rt
         self.b_Rt = b_Rt
         self.a_vac = a_vac
@@ -64,43 +60,38 @@ class Model:
         self.gamma_cutoff = gamma_cutoff
         self.tau_vac1 = tau_vac1
         self.tau_vac2 = tau_vac2
-        self.Theta = Theta
-        self.Theta_ICU = Theta_ICU
-        self.t_max = t_max
-        self.step_size = step_size
-        self.influx = influx
+
+        self.k_ICUcap = k_ICUcap
+        self.epsilon_k = epsilon_k
+        self.k_lowH_NPI = k_lowH_NPI
+        self.k_highH_NPI = k_highH_NPI
+        self.k_lowH_noNPI = k_lowH_noNPI
+        self.k_highH_noNPI = k_highH_noNPI
+
+        self.alpha_u = alpha_u
+        self.alpha_w = alpha_w
+        self.u_base = u_base
+        self.w_base = w_base
+        self.chi_u = chi_u
+        self.chi_w = chi_w
         self.time_u = time_u
         self.time_w = time_w
+        self.epsilon_u = epsilon_u
+        self.epsilon_w = epsilon_w
+
         self.epsilon_free = epsilon_free
-        self.C_base = C_base
-        self.C_npi_base = C_npi_base
-        self.C_npi_free = C_npi_free
-        self.C_vol = C_vol
-        self.w_ICU = w_ICU
-        self.plateaus = plateaus
-        self.slopes = slopes
+        self.influx = influx
+        self.t_max = t_max
+        self.step_size = step_size
         self.feedback_off = feedback_off
-        self.mappingparam = mappingparam
-        self.fit_epsilon = fit_epsilon
-        self.fit_ICUcap = fit_ICUcap
-        self.fit_HR_reduced = fit_HR_reduced
-        self.fit_LR_reduced = fit_LR_reduced
-        self.fit_HR_free = fit_HR_free
-        self.fit_LR_free = fit_LR_free
-        self.Cs = Cs
 
-
-        self.eqs = 18           # number equations
-        self.ags = len(y0)//self.eqs  # number agegroups
+        self.eqs = 18                   # number equations
+        self.ags = len(y0)//self.eqs    # number agegroups
 
         self.M = self.y0.reshape([self.eqs,self.ags])[:-4,:].sum(axis=0)
-        self.u_max = 1-chi_0
-        self.w_max = 1-chi_1
-        self.c_v = 2. / ( self.M*self.omega_v_b*(1/(self.gamma+self.delta+self.Theta)) )
-        self.c_n = 2. / ( self.M*self.omega_n_b*(1/(self.gamma+self.delta+self.Theta)) )
+        self.u_max = 1-chi_u
+        self.w_max = 1-chi_w
         self.t_min = self.gamma_cutoff + max(self.tau_vac1,self.tau_vac2)
-
-        self.n = 1
 
 
     def time2index(self, t):
@@ -111,10 +102,9 @@ class Model:
 
     def H(self,t,tau,a,b):
         x = -np.arange(-self.gamma_cutoff,0,self.step_size)
-        d = self.data[self.time2index(t-self.gamma_cutoff-tau):self.time2index(t-tau),10:12].sum(axis=1)
-        d = (1-self.w_ICU) * d.sum(axis=1) / (self.M.sum()/1e6) + (self.w_ICU * d / (self.M/1e6)).transpose()
-        g = b**a * x**(a-1) * np.e**(-b*x)/gamma_func(a)
-        return (d*g).sum(axis=1)*self.step_size
+        d = self.data[self.time2index(t-self.gamma_cutoff-tau):self.time2index(t-tau),10:12].sum(axis=(1,2))
+        g = b**a * x**(a-1) * np.exp(-b*x) / gamma_func(a)
+        return (d*g).sum()*self.step_size
 
     def H_Rt(self, t):
         return self.H(t, 0, self.a_Rt, self.b_Rt)
@@ -130,82 +120,64 @@ class Model:
 
     def Gamma(self, t):
         return 1 + self.mu*np.cos(2*np.pi*(t+self.d_0-self.d_mu)/360.)
-    
-    #Cosmo Data fit 
-#    def selfregulation(self, t):
-#        return self.plateaus - self.slopes*self.fit_epsilon*np.log(np.e**(1/self.fit_epsilon*(self.fit_ICUcap-self.H_Rt(t)))+1)
 
-#    def alpha_vol(self,t):
-#        return (1-(self.selfregulation(t)-self.mappingparam)/(5-self.mappingparam))
-
-    def fit_HR(self, t):
+    def k_lowH(self, t):
         if t<180-self.epsilon_free:
-            return self.fit_HR_reduced
+            return self.k_lowH_NPI
         if t>180+self.epsilon_free:
-            return self.fit_HR_free
+            return self.k_lowH_noNPI
         else:
-            return self.fit_HR_reduced + (self.fit_HR_free-self.fit_HR_reduced)*(t-180+self.epsilon_free)/2/self.epsilon_free
-    def fit_LR(self, t):
+            return self.k_lowH_NPI + (self.k_lowH_noNPI-self.k_lowH_NPI)*(t-180+self.epsilon_free)/2/self.epsilon_free
+ 
+    def k_highH(self, t):
         if t<180-self.epsilon_free:
-            return self.fit_LR_reduced
+            return self.k_highH_NPI
         if t>180+self.epsilon_free:
-            return self.fit_LR_free
+            return self.k_highH_noNPI
         else:
-            return self.fit_LR_reduced + (self.fit_LR_free-self.fit_LR_reduced)*(t-180+self.epsilon_free)/2/self.epsilon_free
+            return self.k_highH_NPI + (self.k_highH_noNPI-self.k_highH_NPI)*(t-180+self.epsilon_free)/2/self.epsilon_free
 
-    def new_sr(self,t):
-#        a = -(self.fit_LR(t)-self.fit_HR(t))/self.fit_ICUcap
-#        b = self.fit_LR(t)-self.fit_HR(t)
-#        res =  (self.H_Rt(t)[0]<self.fit_ICUcap) * (a*self.H_Rt(t)[0]+b) + self.fit_HR(t)
-        a = (self.fit_LR(t)-self.fit_HR(t))/self.fit_ICUcap
-        # ugly hack: just choose the first entry of H_Rt, FIX IT LATER!
-        res = self.softplus(a, self.fit_HR(t), self.fit_ICUcap, 10)(self.H_Rt(t)[0])
+    def k_selfregulation(self,t):
+        if self.feedback_off:
+            res = self.k_lowH(t)
+        else:
+            a = (self.k_lowH(t)-self.k_highH(t))/self.k_ICUcap
+            res = self.softplus(a, self.k_highH(t), self.k_ICUcap, self.epsilon_k)(self.H_Rt(t))
         # scale down households
         res[0] *= res[1:].sum()/3.
         return res
  
     def Rt(self, t): # not directly used in the code, for plotting only
-        CM = np.zeros([6,6])
-        for i in range(4):
-            CM += self.Cs[i,:,:] * self.new_sr(t)[i]
-        return self.R0 * self.Gamma(t) * max(np.linalg.eigvals(CM))
+        #CM = np.zeros([6,6])
+        #for i in range(4):
+        #    CM += self.Cs[i,:,:] * self.new_sr(t)[i]
+        CM = (np.moveaxis(self.Cs,0,2) * self.k_selfregulation(t)).sum(axis=2)
+        return self.beta / self.gamma * self.Gamma(t) * max(np.linalg.eigvals(CM))
 
     def u_w(self, t):
-        return self.u_base + (self.u_max-self.u_base)*(1-np.exp(-self.alpha_u*self.H_vac1(t)-self.e_u))
+        return self.u_base + (self.u_max-self.u_base)*(1-np.exp(-self.alpha_u*self.H_vac1(t)))
 
     def Phi(self, t, UC, frac):
-#        ppl = (self.u_w(t))*self.M - UC
-#        return ( ppl > 0 ) * ppl * frac / self.time_u
-        return self.softplus(frac/self.time_u, 0, self.u_w(t), 0.01)(UC/self.M) * self.M
+        return self.softplus(frac/self.time_u, 0, self.u_w(t), self.epsilon_u)(UC/self.M) * self.M
 
     def w_w(self, t):
-        return self.w_max*(1-np.exp(-self.alpha_w*self.H_vac2(t)-self.e_w))
+        return self.w_base + (self.w_max-self.w_base)*(1-np.exp(-self.alpha_w*self.H_vac2(t)))
 
     def phi(self, t, UC, WC, frac):
-#        ppl = ((self.w_w(t))*UC - WC)
-#        return ( ppl > 0 ) * ppl * frac / self.time_w
-        return self.softplus(frac/self.time_w, 0, self.w_w(t), 0.01)(WC/UC) * UC
+        return self.softplus(frac/self.time_w, 0, self.w_w(t), self.epsilon_w)(WC/UC) * UC
 
-    def get_phis(self, t, y):   # for plotting only right now; TODO later: check speed and use if not to slow
+    def get_phis(self, t, y):
         (S,V,Wn,Wv,E,EBn,EBv,I,IBn,IBv,ICU,ICUv,R,Rv,UC,WC,D,C) = np.split(y, self.eqs)
         Phi = self.Phi(t, UC, (S+Wn)/(self.M-UC))
         phi = self.phi(t, UC, WC, (Wv)/(UC-WC))
         return Phi, phi
 
-    # ACHTUNG: No reduced waning for high incidences at the moment
-    def omega_v(self, t, I, IBn, IBv):
-        return self.omega_v_b#*(1-1/(1+np.exp(-self.c_v*self.Rt(t)*self.I_eff(I,IBn,IBv))))*2
-
-    def omega_n(self, t, I, IBn, IBv):
-        return self.omega_n_b#*(1-1/(1+np.exp(-self.c_v*self.Rt(t)*self.I_eff(I,IBn,IBv))))*2
-
     def fun(self, t, y):
-        y.reshape([self.eqs,self.ags])
+        #y.reshape([self.eqs,self.ags])
         (S,V,Wn,Wv,E,EBn,EBv,I,IBn,IBv,ICU,ICUv,R,Rv,UC,WC,D,C) = np.split(y, self.eqs)
 
         # definitions to make DEs more readable
         M = self.M
-        eta = self.eta
         rho = self.rho
         kappa = self.kappa
         gamma = self.gamma
@@ -215,61 +187,56 @@ class Model:
         Theta_ICU = self.Theta_ICU
         Rt = self.Rt(t)
         I_eff = self.I_eff(I, IBn, IBv)
-        omega_n = self.omega_n(t, I, IBn, IBv)
-        omega_v = self.omega_v(t, I, IBn, IBv)
-        Phi = self.Phi(t, UC, (S+Wn)/(M-UC))
-        phi = self.phi(t, UC, WC, (Wv)/(UC-WC))
+        omega_n = self.omega_n
+        omega_v = self.omega_v
+        Phi, phi = self.get_phis(t,y)
+#        Phi = self.Phi(t, UC, (S+Wn)/(M-UC))
+#        phi = self.phi(t, UC, WC, (Wv)/(UC-WC))
         
         
         #Effective time dependent contact matrix
-        #CM = self.C_base + self.C_npi(t) + np.maximum(self.alpha_vol(t),self.feedback_off)*self.C_vol
-        CM = np.zeros([6,6])
-        for i in range(4):
-            CM += self.Cs[i,:,:] * self.new_sr(t)[i]
-        #CM = self.new_sr(t) * self.Cs
+        #CM = np.zeros([6,6])
+        #for i in range(4):
+        #    CM += self.Cs[i,:,:] * self.new_sr(t)[i]
 
+        CM = (np.moveaxis(self.Cs,0,2) * self.k_selfregulation(t)).sum(axis=2)
         # PD: CM is no longer symmetric, transposed should be right
-        infect = gamma*self.R0*self.Gamma(t) * np.matmul(CM.transpose(), I_eff/M)
+        infect = self.beta*self.Gamma(t) * np.matmul(CM.transpose(), I_eff/M)
 
 
         # differential equations
         dS = -S*infect - Phi*(S/(S+Wn))
-        dV = -(1-eta)*V*infect + Phi + phi - omega_v*V
+        dV = Phi + phi - omega_v*V
         dWn = -Wn*infect + omega_n*R - Phi*(Wn/(S+Wn))
         dWv = -Wv*infect + omega_v*V + omega_n*Rv - phi
         dE = S*infect - rho*E
-        dEBn = Wn*infect + R*(1-eta)*infect - rho*EBn
-        dEBv = ((1-eta)*(V+Rv)+Wv)*infect - rho*EBv
+        dEBn = Wn*infect - rho*EBn
+        dEBv = Wv*infect - rho*EBv
         dI = rho*E - (gamma+delta+Theta)*I
         dIBn = rho*EBn - (gamma + (Theta+delta)*(1-kappa))*IBn
         dIBv = rho*EBv - (gamma + (Theta+delta)*(1-kappa))*IBv
         dICU = delta*(I + (1-kappa)*IBn) - (Theta_ICU+gamma_ICU)*ICU
         dICUv = delta*(1-kappa)*IBv - (Theta_ICU+gamma_ICU)*ICUv
-        dR = gamma*(I+IBn) - omega_n*R + gamma_ICU*ICU - R*(1-eta)*infect
-        dRv = gamma*IBv - omega_n*Rv + gamma_ICU*ICUv - Rv*(1-eta)*infect
+        dR = gamma*(I+IBn) - omega_n*R + gamma_ICU*ICU
+        dRv = gamma*IBv - omega_n*Rv + gamma_ICU*ICUv
         dUC = Phi
         dWC = phi
         dD = Theta*I + (1-kappa)*Theta*(IBn+IBv) + Theta_ICU*(ICU+ICUv)
-        dC = (S+Wn+Wv+(1-eta)*V)*infect
+        dC = (S+Wn+Wv)*infect
 
         return np.concatenate([dS,dV,dWn,dWv,dE,dEBn,dEBv,dI,dIBn,dIBv,dICU,dICUv,dR,dRv,dUC,dWC,dD,dC]).flatten()
 
+
     def build_data(self):
-        times = np.arange(0,self.t_max,self.step_size)
-        self.data = np.zeros((self.time2index(self.t_max)+100,self.eqs,self.ags))    # solve_ivp tends to look in the future, H_* needs values
+        self.times = np.arange(0,self.t_max,self.step_size)
+        self.data = np.zeros((self.time2index(self.t_max)+100,self.eqs,self.ags))    # solve_ivp tends to look into the future, H needs values
         self.data[:self.time2index(0)+1,:,:] = [self.y0.reshape([self.eqs,self.ags])]*(self.time2index(0)+1)
 
     def run(self):
-        times = np.arange(0,self.t_max,self.step_size)
-        self.data = np.zeros((self.time2index(self.t_max)+100,self.eqs,self.ags))    # solve_ivp tends to look in the future, H_* needs values
-        self.data[:self.time2index(0)+1,:,:] = [self.y0.reshape([self.eqs,self.ags])]*(self.time2index(0)+1)
-
-        for i in range(len(times)-1):
-            res = solve_ivp(self.fun, (times[i],times[i+1]), self.data[self.time2index(i*self.step_size)].flatten())
+        self.build_data()
+        for i in range(len(self.times)-1):
+            res = solve_ivp(self.fun, (self.times[i],self.times[i+1]), self.data[self.time2index(i*self.step_size)].flatten())
             self.data[self.time2index(i*self.step_size)+1,:,:] = res["y"][:,-1:].reshape(self.eqs,self.ags)
-
-        self.times = times
-
         return self.times, self.chopped_data()
 
     def chopped_data(self):
--- code/parameters_agegroups.py
+++ code/parameters_agegroups.py
@@ -3,37 +3,26 @@ import pandas as pd
 
 DEUparams = pd.read_csv('../parameters/DEU_params.csv', sep=';',header=0)
 
-C_base = np.loadtxt('../parameters/C_Base.csv', delimiter=',')
-C_vol = np.loadtxt('../parameters/C_vol.csv', delimiter=',')
-C_npi1 = np.loadtxt('../parameters/C_NPI_general.csv', delimiter=',')
-C_npi2 = np.loadtxt('../parameters/C_NPI_School.csv', delimiter=',')
-C_npi3 = np.loadtxt('../parameters/C_NPI_work.csv', delimiter=',')
-
-
 params_base = {
-    'R0':5.0,
-    'eta': 0.75,                # vac effect against transmission
-    'kappa': 0.8,               # vac effect against severe disease
+    #y0
+    #Cs
+
+    'beta':0.5,
+    'kappa': 0.8,
     'sigma': 0.5,
-    'gamma': 0.1,
-    'gamma_ICU': 0.13,
-    'delta': 0.0019,
-    'rho': 0.25,        
-    'omega_v_b': 1./(8*30),
-    'omega_n_b': 1./(12*30),
-    'chi_0': 0.1,       #
-    'chi_1': 0.2,       #
-    'alpha_w': 0.03,    #
-    'alpha_u': 0.02,    #
-    'alpha_R': 0.01,    #
-    'e_R': 0.,
-    'e_u': 0.,
-    'e_w': 0.,
-    'vac_max': 0.005,   # maximal vaccination rate?
-    'u_base': 0.5,      # add w_base?
-    'mu': 0.267,        # seasonality strength
-    'd_0': 8*30.,       # seasonality start point
-    'd_mu': 0.,         # day of seasonality maximum
+    #'delta': 0.0019,
+    'rho': 0.25,
+    #'gamma': 0.1,
+    #'gamma_ICU': 0.13,
+    #'Theta': 0.0005366,
+    #'Theta_ICU': 0.09755,
+    'omega_v': 1./(4*30),
+    'omega_n': 1./(4*30),
+
+    'mu': 0.267,
+    'd_0': 8*30.,
+    'd_mu': 0.,
+
     'a_Rt': 4.,
     'b_Rt': 0.7,
     'a_vac': 6.,
@@ -41,117 +30,61 @@ params_base = {
     'gamma_cutoff': 45.,
     'tau_vac1': 6*7.,
     'tau_vac2': 2*7.,
-    'Theta':0.0005366,      ###
-    'Theta_ICU':0.09755,    ###
+
+    'k_ICUcap': 35.,
+    'epsilon_k': 10.,
+    #k_lowH_NPI
+    #k_highH_NPI
+    #k_lowH_noNPI
+    #k_highH_noNPI
+
+    #'alpha_u': 0.02,
+    #'alpha_w': 0.03,
+    #'u_base': 0.5,
+    #'w_base': 0.0,
+    #'chi_u': 0.1,
+    #'chi_w': 0.2,
+    'time_u': 7.,
+    'time_w': 7.,
+    'epsilon_u': 0.01,
+    'epsilon_w': 0.01,
+
+    'epsilon_free':14.,
+    'influx': 1,
     't_max': 360.,
     'step_size': 0.1,
-    'influx': 1,
-    'epsilon_free':14.,
-    'w_ICU': 0.,
-    'fit_ICUcap':35,
-    'fit_epsilon':1,
-    'mappingparam':2.5,
+    'feedback_off': False,
 }
-    
-import cosmodata
 
-def get_params(country='Germany', modeltype='base', scenario='scenario2', nr_ages=6):
+def get_params(scen=3):
 
     params = params_base.copy()
+    for i in ['delta', 'gamma', 'gamma_ICU', 'Theta', 'Theta_ICU', 'alpha_u', 'alpha_w', 'u_base', 'w_base', 'chi_u', 'chi_w']:
+        params[i] = DEUparams[i].values
 
-    mapping_modelparams = {'base':{}, 'ramp':params_ramp, 'logistic':params_logistic}
-    params.update(mapping_modelparams[modeltype])
+    params['y0'] = calc_y0(params)
+    params['Cs'] = calc_Cs()
+    params.update( calc_ks(scen) )
 
-    
-    allinitials = initials(params, nr_ages)
-    
-    params.update({'y0':allinitials})
-    
-    for i in ['delta', 'Theta', 'Theta_ICU', 'gamma', 'gamma_ICU', 'alpha_R', 'alpha_u', 'alpha_w', 'chi_0', 'chi_1', 'u_base']:
-        params.update({i: np.array(DEUparams[i])})
-        
-
-    matrices = contactmatrices(scenario)
-    
-    params.update(matrices)
-    
-    
-    plateaus = cosmodata.plateaus_6agegroups
-    slopes = cosmodata.slopes_6agegroups
-    
-    params['plateaus'] = plateaus
-    params['slopes'] = slopes
-    params['feedback_off'] = False
-    
     return params 
 
 
-def initials(params, nr_ages):
-    y0allages = []
-    
-    for ag in range(nr_ages):
-        newparams = updateparams(params,ag)
-        inits = calc_y0(newparams, ag)
-        y0allages.append(inits)
+def calc_y0(params):
 
-    
-    
-    return np.array(y0allages).flatten(order='F')
+    darkfigure = 2.36
 
-def contactmatrices(scenario):
-    
-    dictionary = {
-        'C_base' : C_base,
-        'C_vol' : C_vol,
-        'C_npi_free' : C_npi1 + C_npi2 + C_npi3
-    }
-    if scenario == 'scenario1': #Close everything 
-        dictionary['C_npi_base'] = np.zeros([6,6])
-    if scenario == 'scenario2': # Leave work open (close schools) 
-        dictionary['C_npi_base'] = C_npi3
-    if scenario == 'scenario3': # Freedom day 
-        dictionary['C_npi_base'] = C_npi1 + C_npi2 + C_npi3
-                    
-    return dictionary
-
-
-def updateparams(params, agegroup):
-    agespecificparams = {
-        'delta': DEUparams.values[agegroup, 3],
-        'Theta': DEUparams.values[agegroup, 4],
-        'Theta_ICU': DEUparams.values[agegroup, 5],
-        'gamma': DEUparams.values[agegroup, 6],
-        'gamma_ICU': DEUparams.values[agegroup, 7],
-        'alpha_R': DEUparams.values[agegroup, 8],
-        'alpha_u': DEUparams.values[agegroup, 9],
-        'alpha_w': DEUparams.values[agegroup, 10],
-        'chi_0': DEUparams.values[agegroup, 11],
-        'chi_1': DEUparams.values[agegroup, 12],
-        'u_base': DEUparams.values[agegroup, 13],
-    }
-    
-    params.update(agespecificparams)
-    
-    return params
-    
-
-def calc_y0(params, agegroup):
-    Mi = DEUparams.values[agegroup, 1]
-    
-    V_raw = DEUparams.values[agegroup, 14]
-    R_raw = DEUparams.values[agegroup, 15]
-    cases = DEUparams.values[agegroup, 16]
-    ICU_raw = DEUparams.values[agegroup, 17]
-    W_V = DEUparams.values[agegroup, 18]
-    W_R = DEUparams.values[agegroup, 19]
-    serop = DEUparams.values[agegroup, 20]
-    
-    darkfigure = serop / (R_raw/1e6)
+    M = DEUparams['Anteil'].values * 1e6
+    V_raw = DEUparams['V_raw'].values
+    R_raw = DEUparams['R_raw'].values
+    cases = DEUparams['cases'].values
+    ICU_raw = DEUparams['ICU'].values
+    W_V = DEUparams['W_V'].values
+    W_R = DEUparams['W_R'].values
 
 
     RRv = darkfigure*(R_raw-W_R)
 
-    V = V_raw - W_V - RRv*((V_raw-W_V)/(Mi*1e6))
+    V = V_raw - W_V - RRv*((V_raw-W_V)/(M))
 
 
     Wn = darkfigure*W_R
@@ -160,150 +93,106 @@ def calc_y0(params, agegroup):
     Etot = 1./params['rho']*cases*darkfigure
     Itot = 1./(params['gamma']+params['delta']+params['Theta'])* cases*darkfigure
 
-    S = 1e6*Mi - Etot - Itot - ICU_raw - V - RRv - Wn - Wv
+    S = M - Etot - Itot - ICU_raw - V - RRv - Wn - Wv
 
 
-    Eimmune =  (1-params['eta'])*(V)
+    En = (Wn) / (S+Wn+Wv) * Etot
+    Ev = (Wv) / (S+Wn+Wv) * Etot
+    E  =  (S) / (S+Wn+Wv) * Etot
 
-    En = (Wn)/(S+Wn+Wv+Eimmune)*Etot
-    Ev = (Wv+Eimmune)/(S+Wn+Wv+Eimmune)*Etot
-    E = Etot - En - Ev
-
-    In = (Wn)/(S+Wn+Wv+Eimmune)*Itot
-    Iv = (Wv+Eimmune)/(S+Wn+Wv+Eimmune)*Itot
-    I = Itot - In -Iv
+    In = (Wn) / (S+Wn+Wv) * Itot
+    Iv = (Wv) / (S+Wn+Wv) * Itot
+    I  =  (S) / (S+Wn+Wv) * Itot
 
 
     ICUimmune = (1-params['kappa'])*(In+Iv)
 
-    ICUv = Iv*(1-params['kappa'])/(I+ ICUimmune)*ICU_raw
-    ICU = ICU_raw - ICUv
+    ICUv =   (Iv*(1-params['kappa'])) / (I+ICUimmune) * ICU_raw
+    ICU  = (I+In*(1-params['kappa'])) / (I+ICUimmune) * ICU_raw
 
     #Nur ne Approximation da Leute wegsterben in D
-    Rimmune = S + Wn + Wv + (1-params['eta'])*V
-
-    R = RRv * (S + Wn)/Rimmune
-    Rv = RRv - R
+    R  = (S+Wn) / (S+Wn+Wv) * RRv
+    Rv =   (Wv) / (S+Wn+Wv) * RRv
 
 
     y0= {
-            'V': V,
-            'Wn': Wn,
-            'Wv': Wv,
-            'E': E,
+            'V':   V,
+            'Wn':  Wn,
+            'Wv':  Wv,
+            'E':   E,
             'EBn': En,
             'EBv': Ev,
-            'I': I,
+            'I':   I,
             'IBn': In,
             'IBv': Iv,
             'ICU': ICU,
-            'ICUv': ICUv,
-            'R': R,
-            'Rv': Rv,
-            'UC': V_raw,
-            'WC': 0.,
-            'D': 0.,
-            'C': 0.,
+            'ICUv':ICUv,
+            'R':   R,
+            'Rv':  Rv,
+            'UC':  V_raw,
+            'WC':  [0.]*6,
+            'D':   [0.]*6,
+            'C':   [0.]*6,
         }
 
 
-    S = Mi*1e6 - (sum(y0.values())-y0['UC'])
-
-
+    S = M - (sum(y0.values())-y0['UC']-y0['WC'])
     y0.update({'S':S})
     
     y0_array = [y0['S'],y0['V'],y0['Wn'],y0['Wv'],y0['E'],y0['EBn'],y0['EBv'],y0['I'],y0['IBn'],y0['IBv'],
                 y0['ICU'],y0['ICUv'],y0['R'],y0['Rv'],y0['UC'],y0['WC'],y0['D'],y0['C']]
     
-    return y0_array
+    return np.array(y0_array).flatten()
 
 
+def calc_Cs():
+    C_household = np.loadtxt('../parameters/Germany_country_level_F_household_setting_85.csv', delimiter=',')
+    C_school = np.loadtxt('../parameters/Germany_country_level_F_school_setting_85.csv', delimiter=',')
+    C_workplace = np.loadtxt('../parameters/Germany_country_level_F_work_setting_85.csv', delimiter=',')
+    C_community = np.loadtxt('../parameters/Germany_country_level_F_community_setting_85.csv', delimiter=',')
+    
+    w_h = 4.1100
+    w_s = 11.4100
+    w_w = 8.0700
+    w_c = 2.7900
+    
+    ind = [0, 19, 39, 59, 69, 79, -1];
+    
+    C_H = np.zeros([6,6])
+    C_S = np.zeros([6,6])
+    C_W = np.zeros([6,6])
+    C_C = np.zeros([6,6])
+    
+    for i in range(6):
+        for j in range(6):
+            C_H[i,j] = C_household[ind[i]:ind[i+1],ind[j]:ind[j+1]].sum()
+            C_S[i,j] = C_school[ind[i]:ind[i+1],ind[j]:ind[j+1]].sum()
+            C_W[i,j] = C_workplace[ind[i]:ind[i+1],ind[j]:ind[j+1]].sum()
+            C_C[i,j] = C_community[ind[i]:ind[i+1],ind[j]:ind[j+1]].sum()
+    
+    C = w_h*C_H + w_s*C_S + w_w*C_W + w_c*C_C
+    rho = max(np.linalg.eigvals(C))
+    
+    C_H = w_h * C_H / rho
+    C_S = w_s * C_S / rho
+    C_W = w_w * C_W / rho
+    C_C = w_c * C_C / rho
+    
+    Cs = np.array([C_H, C_S, C_W, C_C])
 
-# Values from OWD
-raw_deu = {
-    'V_raw': 603900.0,
-    'R_raw':  47400.0,
-    'cases':    116.25,
-    'ICU':       13.46,
-    'W_V':    18000.0,
-    'W_R':     9030.0,
-    'serop':      0.112,
-}
-
-raw_cze = {
-    'V_raw': 536500.0,
-    'R_raw': 156600.0,
-    'cases':     18.56,
-    'ICU':        0.84,
-    'W_V':    14650.0,
-    'W_R':    29100.0,
-    'serop':      0.51, #Feb and March 2021; 15. March reported = 0.13
-}
-
-raw_dsk = {
-    'V_raw': 725400.0,
-    'R_raw':  59800.0,
-    'cases':    147.03,
-    'ICU':        4.3,
-    'W_V':    22450.0,
-    'W_R':    11610.0,
-    'serop':      0.173, #Dec 2020 serop=0.067, 30. Dec reported=0.0278
-}
-
-raw_por = {
-    'V_raw': 755500.0,
-    'R_raw': 102200.0,
-    'cases':    191.4,
-    'ICU':       12.88,
-    'W_V':    18550.0,
-    'W_R':    18400.0,
-    'serop':      0.173,
-}
+    return Cs
 
 
+def calc_ks(scen):
+    ks = pd.read_csv('../parameters/scenariosDefinition.csv', sep=';')
+    ks_NPI = ks.loc[ks['Scenarios']==scen]
+    ks_noNPI = ks.loc[ks['Scenarios']==1]
 
+    d = {}
+    d['k_lowH_NPI']    = np.array([ks_NPI['householdhigh'],  ks_NPI['schoolhigh'],  ks_NPI['workplacehigh'],  ks_NPI['communityhigh']]).flatten()
+    d['k_highH_NPI']   = np.array([ks_NPI['householdlow'],   ks_NPI['schoollow'],   ks_NPI['workplacelow'],   ks_NPI['communitylow']]).flatten()
+    d['k_lowH_noNPI']  = np.array([ks_noNPI['householdhigh'],ks_noNPI['schoolhigh'],ks_noNPI['workplacehigh'],ks_noNPI['communityhigh']]).flatten()
+    d['k_highH_noNPI'] = np.array([ks_noNPI['householdlow'], ks_noNPI['schoollow'], ks_noNPI['workplacelow'], ks_noNPI['communitylow']]).flatten()
 
-params_cze = {
-    'delta': 0.001717,
-    'Theta':0.00040588,
-    'Theta_ICU':0.0978747,
-}
+    return d
 
-params_dsk = {
-    'delta': 0.0017,
-    'Theta':0.000429676,
-    'Theta_ICU':0.09786686,
-}
-
-params_por = {
-    'delta': 0.0019083,
-    'Theta':0.000523277,
-    'Theta_ICU':0.097566211,
-}
-
-params_ramp = {
-    'time_u':7.,
-    'time_w':7.,
-}
-
-params_logistic = {
-    'r_base':0.015,
-}
-
-
-Rtbase = {
-    'scenario1':5,
-    'scenario2':3.75,
-    'scenario3':2.5,
-}
-
-
-tmp = np.array([[0.11379518, 0.12928566, 0.09578086, 0.07168928, 0.04753613, 0.03652391],
-[0.20671978, 0.13455128, 0.17471492, 0.09173655, 0.05707505, 0.0366261],
-[0.09233393, 0.13444288, 0.17963023, 0.18572315, 0.11123063, 0.0440081],
-[0.06793128, 0.05804828, 0.14403942, 0.38214947, 0.28185652, 0.12505345],
-[0.05425213, 0.04132636, 0.10075469, 0.34296702, 0.35412762, 0.15467227],
-[0.05417458, 0.03423004, 0.05199785, 0.19549228, 0.19778876, 0.69455006]])
-CM_DEU = np.flip(np.flip(tmp, axis=0), axis=1)
-
-CM_uniform = 1/6*np.ones([6,6])
